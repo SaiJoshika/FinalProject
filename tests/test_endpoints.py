9a9b2e5920e8b2950964e7e7f11f180139cf9794
SaiJoshika <saijoshikareddy@gmail.com>
2023-12-15 21:38:50 -0600

    Initial commit: Project setup

--- tests/test_endpoints.py
+++ tests/test_endpoints.py
@@ -1,49 +1,44 @@
-import unittest
+import pytest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
-class TestEndpoints(unittest.TestCase):
-    def setUp(self):
-        """Set up test client."""
-        self.app = app.test_client()
+@pytest.fixture
+def client():
+    with app.test_client() as client:
+        yield client
 
-    def tearDown(self):
-        """Tear down resources after each test."""
-        # Add teardown logic if necessary
+def test_get_all_listings_successful(client):
+    response = client.get('/listings')
+    assert response.status_code == 200
+    
+def test_get_all_listings_failure(client):
+    response = client.get('/listings')
+    assert response.status_code == 200 
+    assert 'error' not in response.json
 
-    def test_get_listings(self):
-        """Test GET /listings."""
-        # Test implementation
-        response = self.app.get('/listings')
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your test requirements
+def test_get_listing_by_id_success(client):
+    listing_id = 323733
+    response = client.get(f'/listings/{listing_id}')
+    assert response.status_code == 200
+    assert response.json is not None
+    assert response.json['id'] == listing_id
+   
+def test_get_listing_by_id_not_found(client):
+    listing_id = 999999 
+    response = client.get(f'/listings/{listing_id}')
+    assert response.status_code == 404
+    assert response.json['error'] == 'Listing not found'
 
-    def test_post_listing(self):
-        """Test POST /listings."""
-        # Test implementation
-        response = self.app.post('/listings', data={'key': 'value'})
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your test requirements
+def test_filter_listings_success(client):
+    response = client.get('/listings/filter?neighbourhood=78704&host_id=1798084&room_type=Entire home')
+    assert response.status_code == 200
+    assert response.json is not None
+    assert isinstance(response.json, list)
 
-    def test_patch_listing(self):
-        """Test PATCH /listings/<id>."""
-        # Test implementation
-        response = self.app.patch('/listings/1', data={'key': 'new_value'})
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your test requirements
-
-    def test_delete_listing(self):
-        """Test DELETE /listings/<id>."""
-        # Test implementation
-        response = self.app.delete('/listings/1')
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your test requirements
-
-    def test_search_listing(self):
-        """Test POST /listing/search."""
-        # Test implementation
-        response = self.app.post('/listing/search', json={'term': 'search_term'})
-        self.assertEqual(response.status_code, 200)
-        # Add more assertions based on your test requirements
-
-if __name__ == '__main__':
-    unittest.main()
+def test_filter_listings_invalid_parameters_bad_request(client):
+    response = client.get('/listings/filter?invalid_param=value')
+    assert response.status_code == 400
+    assert 'error' in response.json
+    assert response.json['error'] == 'Invalid query parameters'
